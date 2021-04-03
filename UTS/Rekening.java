5729940699c2ca2588400d3ecbdd3996f85f5433
AuliaGustiRahman <gustirahman0710@gmail.com>
2021-04-03 10:43:18 +0700

    update

---
+++ UTS/Rekening.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ACER
+ */
+public class Rekening {
+    String noRekening, namaBelakang, namaDepan, phone , email, saldoAwal, saldoAkhir, tanggalTransaksi;
+    
+    Rekening (String nr, String nb, String nd, String p, String e);
+        noRekening = nr;
+        namaBelakang = nb;
+        namaDepan = nd;
+        phone = p;
+        email = e;
+}
